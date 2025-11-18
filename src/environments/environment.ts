8602f2c3a5f615326aeb1afd00565ca78b365df6
Raciel Reyes Blanco <racielreyes@hotmail.es>
2025-11-18 09:58:03 +0100

    creado ambiente prod1

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
-  SERVER_URL:"https://amigo-invisible-backend.onrender.com",
-  CLIENT_URL:"https://cena24.netlify.app"
+  production:true,
+  SERVER_URL:"https://amigo-invisible-backend-1.onrender.com",
 };
